c5fac397327ea0443a97c4d7e683c5031ceb4918
Dakusy <dakusy@gmail.com>
2023-05-18 19:30:27 +0200

    fix name profil

--- sportsee/src/components/Profil.jsx
+++ sportsee/src/components/Profil.jsx
@@ -4,7 +4,7 @@ import "../css/Profil.css"
 function Profil({ user }) {
   return (
     <div className="profil-container">
-      <h1>Bonjour {user?.firstName}</h1>
+      <h1>Bonjour <span class="name">{user?.firstName}</span></h1>
       <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
     </div>
   );
